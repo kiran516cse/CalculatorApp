358a7f8fe092e3fc8605dd1c12cf79bfc6f7ba3a
kiran516cse <kirankumar.k@nichi.com>
2024-05-06 11:32:14 +0900

    Base structure for Calculator App

--- CalculatorApp/UI/Calculator.cs
+++ CalculatorApp/UI/Calculator.cs
@@ -1,8 +1,8 @@
 namespace CalculatorApp
 {
-    public partial class Form1 : Form
+    public partial class Calculator : Form
     {
-        public Form1()
+        public Calculator()
         {
             InitializeComponent();
         }
